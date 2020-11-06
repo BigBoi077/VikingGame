53c9ee2e92fc08dbcd49b3297737d232755b5aca
BigBoi077 <58580199+BigBoi077@users.noreply.github.com>
2020-11-06 15:01:32 -0500

    Initial commit

---
+++ src/cegepst/Player.java
@@ -0,0 +1,25 @@
+import cegepst.engine.Buffer;
+import cegepst.engine.controls.MovementController;
+import cegepst.engine.entity.ControllableEntity;
+
+import java.awt.*;
+
+public class Player extends ControllableEntity {
+
+    public Player(MovementController controller) {
+        super(controller);
+        setSpeed(3);
+        setDimensions(32, 32);
+    }
+
+    @Override
+    public void update() {
+        super.update();
+        moveAccordingToHandler();
+    }
+
+    @Override
+    public void draw(Buffer buffer) {
+        buffer.drawRectangle(x, y, width, height, Color.green);
+    }
+}
